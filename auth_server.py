e6b4fdfb4d6f052772eac22129fa11c04c94d76f
ateraz <ateraz@example.com>
2014-04-01 15:45:43 -0700

    Add ip checker service

--- auth_server.py
+++ auth_server.py
@@ -4,11 +4,43 @@ from twisted.protocols.basic import LineReceiver
 from twisted.python import log, usage, failure
 from twisted.enterprise import adbapi
 from datetime import datetime
-from base64 import b64decode
-import yaml
+import yaml, functools, base64
 
 
-class AuthServerOptions(usage.Options):
+class BaseProtocol(LineReceiver):
+
+    #delimiter = b'\n'
+
+    def lineReceived(self, line):
+        self.factory.lineReceived(line).addCallback(self.sendLine)
+
+
+# Ihnerit from object to enable call of super() in subclasses
+class BaseFactory(protocol.ServerFactory, object):
+
+    protocol = BaseProtocol
+
+    def __init__(self, service):
+        self.service = service
+
+    def lineReceived(self, line):
+        # Need to be implemented in subclasses
+        pass
+
+
+class ValidatorFactory(BaseFactory):
+
+    def lineReceived(self, line):
+        return self.service.validate(line)
+
+
+class CheckerFactory(BaseFactory):
+
+    def lineReceived(self, line):
+        return self.service.check(line)
+
+
+class AuthOptions(usage.Options):
 
     optParameters = [
         ['config_path', 'c', 'settings.yml', 'Configuration file path.']]
@@ -23,51 +55,44 @@ class AuthConfigurator(object):
         return self.config[key]
 
 
-class AuthProtocol(LineReceiver):
-
-    delimiter = b'\n'
-
-    def lineReceived(self, auth_str):
-        self.factory.service.validate(auth_str, self.writeResponse)
-
-    def writeResponse(self, user):
-        self.sendLine('OK' if user else 'ERR')
-
-
-class AuthFactory(protocol.ServerFactory):
-
-    protocol = AuthProtocol
-
-    def __init__(self, service):
-        self.service = service
-
-
 class AuthService(service.Service):
+
+    successResponse = 'OK'
+    failResponse = 'ERR'
 
     def __init__(self, config):
         self.users, self.config = {}, config
 
     def startService(self):
         service.Service.startService(self)
-        db = self.config['database']
-        self.db = adbapi.ConnectionPool('MySQLdb', db=db['name'],
-                                        user=db['user'], passwd=db['pass'])
+        conf = self.config['database']
+        self.db = adbapi.ConnectionPool('MySQLdb', db=conf['name'],
+                                        user=conf['user'], passwd=conf['pass'])
 
-    def validate(self, auth_str, auth_callback):
-        user = self.getUserParams(auth_str)
+    def validate(self, line):
+        user = self.getUserParams(line)
         if not user:
-            return auth_callback(None)
+            d = defer.Deferred()
+            d.callback(self.failResponse)
+            return d
         d = self.db.runQuery(
             'SELECT id, login FROM users_all WHERE login=%s AND password=%s',
             (user['login'], user['password']))
-        d.addCallback(self.checkUser, user)
-        d.addCallbacks(self.successCallback, self.failCallback)
-        d.addCallback(auth_callback)
+        d.addCallback(self.validateUser, user)
+        return d.addCallbacks(self.successCallback, self.failCallback)
 
-    def checkUser(self, results, user):
+    def check(self, ip):
+        d = defer.Deferred()
+        user_id = '0'
+        if ip in self.users:
+            user_id = self.users[ip][1]
+        d.callback(str(user_id))
+        return d
+
+    def validateUser(self, results, user):
         ip, login = user['ip'], user['login']
         if len(results) == 0 or (ip in self.users and
-                    self.users[ip][0] != login):
+                self.users[ip][0] != login):
             return failure.Failure(user)
         userid = results[0][0]
         self.users[user['ip']] = (user['login'], userid)
@@ -77,18 +102,18 @@ class AuthService(service.Service):
     def successCallback(self, user):
         utils.getProcessValue(
             self.config['auth_callback']['success'].format(**user))
-        return user
+        return self.successResponse
 
     def failCallback(self, user):
         utils.getProcessValue(
             self.config['auth_callback']['fail'].format(**user))
-        # Method shouldn't return anything but None
+        return self.failResponse
 
     @staticmethod
     def getUserParams(auth_str):
         parts = auth_str.replace('%20', ' ').split()
         try:
-            credentials = b64decode(parts[2]).split(':')
+            credentials = base64.b64decode(parts[2]).split(':')
         except:
             log.msg('Bad auth string format')
             return None
--- twisted/plugins/squid_auth.py
+++ twisted/plugins/squid_auth.py
@@ -1,13 +1,14 @@
 from zope.interface import implements
 from twisted.plugin import IPlugin
 from twisted.application import internet, service
-from auth_server import AuthService, AuthFactory, AuthServerOptions, AuthConfigurator
+from auth_server import (AuthService, AuthOptions, AuthConfigurator,
+    ValidatorFactory, CheckerFactory)
 
 class AuthServiceMaker(object):
     implements(service.IServiceMaker, IPlugin)
     tapname = 'squid_auth'
     description = 'A service for IP based squid auth and session checking.'
-    options = AuthServerOptions
+    options = AuthOptions
 
     def makeService(self, options):
         top_service = service.MultiService()
@@ -16,10 +17,15 @@ class AuthServiceMaker(object):
         auth_service = AuthService(config)
         auth_service.setServiceParent(top_service)
 
-        tcp_service = internet.TCPServer(
-            int(config['server']['port']), AuthFactory(auth_service),
-            interface=config['server']['interface'])
-        tcp_service.setServiceParent(top_service)
+        validator = internet.TCPServer(
+            int(config['validator']['port']), ValidatorFactory(auth_service),
+            interface=config['validator']['interface'])
+        validator.setServiceParent(top_service)
+
+        checker = internet.TCPServer(
+            int(config['checker']['port']), CheckerFactory(auth_service),
+            interface=config['checker']['interface'])
+        checker.setServiceParent(top_service)
 
         return top_service
 
