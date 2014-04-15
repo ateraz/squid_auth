c64afd5239e3772e9d0544bb11aa0c625a56383b
ateraz <ateraz@example.com>
2014-04-15 13:19:25 -0700

    Make server prototypes more similar

---
+++ benchmark/async_server.py
@@ -0,0 +1,28 @@
+"""Prototype of async tcp server"""
+
+from twisted.internet import reactor, protocol
+from twisted.enterprise import adbapi
+
+
+class Connection(protocol.Protocol):
+
+    db = adbapi.ConnectionPool(
+        'MySQLdb', db='squid_auth', user='root',
+        passwd='root', host='localhost')
+
+    def dataReceived(self, data):
+        self.db.runQuery(
+            'SELECT * FROM users_all WHERE login=%s AND passwd=%s',
+            ('login', 'pass')
+        ).addCallback(self.respond)
+
+    def respond(self, _):
+        self.transport.write('OK')
+        self.transport.loseConnection()
+
+
+if __name__ == '__main__':
+    factory = protocol.ServerFactory()
+    factory.protocol = Connection
+    reactor.listenTCP(9999, factory)
+    reactor.run()
--- benchmark/blocking_server.py
+++ benchmark/blocking_server.py
@@ -1,22 +1,36 @@
-import MySQLdb, socket
+"""Prototype of blocking tcp server"""
 
-def respond(sock, cursor):
-    sock.recv(1024)
-    cursor.execute('SELECT * FROM users_all WHERE login=%s AND passwd=%s',
-        ('user', 'pass'))
-    print cursor.fetchone()
-    sock.sendall('OK\n')
-    sock.close()
+import MySQLdb
+import socket
+
+
+class Connection:
+
+    db = MySQLdb.connect(
+        db='squid_auth', user='root',
+        passwd='root', host='localhost'
+    ).cursor()
+
+    def __init__(self, sock):
+        self.sock = sock
+
+    def dataReceived(self):
+        self.sock.recv(1024)
+        self.db.execute(
+            'SELECT * FROM users_all WHERE login=%s AND passwd=%s',
+            ('login', 'pass'))
+        self.respond(self.db.fetchone())
+
+    def respond(self, _):
+        self.sock.sendall('OK')
+        self.sock.close()
 
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(db='squid_auth', user='root', passwd='root')
-    cursor = db.cursor()
-
     listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     listen_socket.bind(('localhost', 9999))
-    listen_socket.listen(100)
+    listen_socket.listen(1000)
     while True:
         sock, addr = listen_socket.accept()
-        respond(sock, cursor)
+        Connection(sock).dataReceived()
